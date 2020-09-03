240ccca7187fa1c19fdd9e1a2dfa541bd0e66f46
Sut103 <kou.103.kan@gmail.com>
2020-09-03 05:13:07 +0000

    一覧を作成

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render, HttpResponse
+from django.template import loader
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, World")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'polls/index.html', context)
 
-def detail(request,question_id):
+
+def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
-def results(request,question_id):
+
+def results(request, question_id):
     return HttpResponse("You're looking at the results of question %s." % question_id)
 
-def vote(request,question_id):
+
+def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
