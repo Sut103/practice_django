ba27c7d8c5d5db2d0cae687a5cba189215ff9682
Sut103 <kou.103.kan@gmail.com>
2020-09-03 09:08:42 +0000

    urlのハードコードを修正

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,10 @@
 from . import views
 from django.urls import path
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
-    path("<int:question_id>/", views.detail, name='datail'),
+    path("<int:question_id>/", views.detail, name='detail'),
     path("<int:question_id>/results/", views.results, name='results'),
-    path("<int:question_id>/vote/", views.vote, name='vote'),
+    path("specifies/<int:question_id>/vote/", views.vote, name='vote'),
 ]
