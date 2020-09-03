ab675f5a6126e2f615af5f4a474d1b9050254970
Sut103 <kou.103.kan@gmail.com>
2020-09-03 23:58:35 +0900

    汎用viewに変更

--- polls/urls.py
+++ polls/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path("<int:question_id>/", views.detail, name='detail'),
-    path("<int:question_id>/results/", views.results, name='results'),
-    path("<int:question_id>/vote/", views.vote, name='vote'),
+    path('', views.IndexView.as_view(), name='index'),  # GET
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),  # GET
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),  # GET
+    path('<int:question_id>/vote/', views.vote, name='vote'),  # POST
 ]
