f97cfed94d02a077857f21d4d420707498e7847d
Sut103 <kou.103.kan@gmail.com>
2020-09-03 09:08:06 +0000

    detail周りを編集

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from django.template import loader
+from django.http import Http404
 
 from .models import Question
 
@@ -13,7 +14,8 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
